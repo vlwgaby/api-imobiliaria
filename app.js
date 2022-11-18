d6fb109782c914de832f3b5d0e61e306d807c446
Gaby França <gabriellyfranca810@gmail.com>
2022-11-18 00:01:58 -0300

    adicionando cors

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
+const cors = require('cors');
+
 
 const port = process.env.PORT || 3000;
 
+server.use(cors());
 server.use(middlewares);
 server.use(router);
 
